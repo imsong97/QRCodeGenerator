0f29ddda63ce605496214a9d7ec9d4e6a17d2902
DyNamiC <imsong1997@gmail.com>
2020-12-18 11:29:40 +0900

    QR Generator

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const qrcode = new QRCode("qrcode");
+
+function onClick(){
+    let date = new Date();
+    
+    const id = document.getElementById("id").value;
+    const serialNumber = document.getElementById("serialNumber").value;
+    const str = {userId:id, SerialNumber:serialNumber, date};
+
+    qrcode.makeCode(JSON.stringify(str));
+}
